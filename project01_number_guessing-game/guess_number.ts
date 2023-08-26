3d526da31ede6460454ebe3e177462ec8c791835
saleempirwani <msr.pirwani@gmail.com>
2023-08-26 16:20:30 +0500

    guess no. updt

--- project01_number_guessing-game/guess_number.ts
+++ project01_number_guessing-game/guess_number.ts
@@ -1,11 +1,20 @@
 import inquirer from "inquirer";
 import chalk from "chalk";
 
+// Types
+interface IAnswer {
+  n1: number;
+}
+
+type Guess = (num: number) => void;
+
+// Console functions
 const log = console.log;
 
+// Generating random number
 const GUESS_NO = Math.round(Math.random() * 9);
 
-const guess = (num: number): void => {
+const guess: Guess = (num) => {
   if (Number.isNaN(num)) log("❌", chalk.red("Invalid number type"));
   else if (num === GUESS_NO) log("✅", chalk.green("Yahh... You won the game"));
   else
@@ -14,7 +23,7 @@ const guess = (num: number): void => {
 
 async function main() {
   try {
-    const answers = await inquirer.prompt([
+    const answers: IAnswer = await inquirer.prompt([
       {
         type: "number",
         name: "n1",
